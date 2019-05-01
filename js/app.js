1567226624005e7a9ddc5097d5241c2d02aa2df1
Harry Northam <harrynortham@icloud.com>
2019-05-01 18:54:06 +0200

    first charts added

---
+++ js/app.js
@@ -0,0 +1,75 @@
+
+var ctx = document.getElementById('myChart').getContext("2d");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F','S'],
+        datasets: [{
+            label: '# of Visits',
+            data: [60, 80, 175, 110, 210, 190,75],
+            backgroundColor: [
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)'
+            ],
+            hoverBackgroundColor: [
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)',
+              'rgba(115, 119, 191, .8)'
+            ]
+        }]
+    },
+    options: {
+
+      legend: {
+            display: false
+          },
+      tooltips: {
+        enabled: false
+      },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+var ctx2 = document.getElementById('myChart2').getContext("2d");
+var myPieChart = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['Phones', 'Tablets', 'Desktops'],
+        datasets: [{
+            label: '# of Visits',
+            data: [70, 40, 175],
+            backgroundColor: [
+                'rgba(129, 201, 143, 1)',
+                'rgba(116, 177, 191, 1)',
+                'rgba(115, 119, 191, 1)'
+            ],
+            hoverBackgroundColor: [
+              'rgba(129, 201, 143, .8)',
+              'rgba(116, 177, 191, .8)',
+              'rgba(115, 119, 191, .8)'
+            ]
+        }]
+    },
+    options: {
+
+      legend: {
+            position: 'right'
+          }
+        }
+});
