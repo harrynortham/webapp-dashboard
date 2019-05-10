ff8b8095409f3d33447a595076bd77834cfd719d
Harry Northam <harrynortham@icloud.com>
2019-05-10 15:52:30 +0200

    added code for alerts modal

--- js/app.js
+++ js/app.js
@@ -1,48 +1,68 @@
+//Make alert box hideable
 
-var ctx = document.getElementById('myChart').getContext("2d");
+const alert = document.querySelector(".alert");
+const alertClose = alert.querySelector(".fa-times");
+
+alertClicked = localStorage.getItem("alertClicked");
+alert.style.display = "block";
+if (alertClicked === "true") {
+  alert.style.display = "none";
+} else {
+  alertClose.addEventListener("click", () => {
+    localStorage.setItem("alertClicked", "true");
+    alert.style.display = "none";
+  });
+}
+
+//charts
+
+var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F','S'],
-        datasets: [{
-            label: '# of Visits',
-            data: [60, 80, 175, 110, 210, 190,75],
-            backgroundColor: [
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)'
-            ],
-            hoverBackgroundColor: [
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)',
-              'rgba(115, 119, 191, .8)'
-            ]
-        }]
+  type: "bar",
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [
+      {
+        label: "# of Visits",
+        data: [60, 80, 175, 110, 210, 190, 75],
+        backgroundColor: [
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)",
+          "rgba(115, 119, 191, 1)"
+        ],
+        hoverBackgroundColor: [
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)",
+          "rgba(115, 119, 191, .8)"
+        ]
+      }
+    ]
+  },
+  options: {
+    legend: {
+      display: false
     },
-    options: {
-
-      legend: {
-            display: false
-          },
-      tooltips: {
-        enabled: false
-      },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+    tooltips: {
+      enabled: false
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
         }
+      ]
     }
+  }
 });
 
 //get navigation items
@@ -53,31 +73,49 @@ var myChart = new Chart(ctx, {
 
 //maybe use case
 
-
-var ctx2 = document.getElementById('myChart2').getContext("2d");
+var ctx2 = document.getElementById("myChart2").getContext("2d");
 var myPieChart = new Chart(ctx2, {
-    type: 'doughnut',
-    data: {
-        labels: ['Phones', 'Tablets', 'Desktops'],
-        datasets: [{
-            label: '# of Visits',
-            data: [70, 40, 175],
-            backgroundColor: [
-                'rgba(129, 201, 143, 1)',
-                'rgba(116, 177, 191, 1)',
-                'rgba(115, 119, 191, 1)'
-            ],
-            hoverBackgroundColor: [
-              'rgba(129, 201, 143, .8)',
-              'rgba(116, 177, 191, .8)',
-              'rgba(115, 119, 191, .8)'
-            ]
-        }]
-    },
-    options: {
+  type: "doughnut",
+  data: {
+    labels: ["Phones", "Tablets", "Desktops"],
+    datasets: [
+      {
+        label: "# of Visits",
+        data: [70, 40, 175],
+        backgroundColor: [
+          "rgba(129, 201, 143, 1)",
+          "rgba(116, 177, 191, 1)",
+          "rgba(115, 119, 191, 1)"
+        ],
+        hoverBackgroundColor: [
+          "rgba(129, 201, 143, .8)",
+          "rgba(116, 177, 191, .8)",
+          "rgba(115, 119, 191, .8)"
+        ]
+      }
+    ]
+  },
+  options: {
+    legend: {
+      position: "right"
+    }
+  }
+});
 
-      legend: {
-            position: 'right'
-          }
-        }
+//modal box
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+closeButton.addEventListener("click", () => {
+  toggleModal();
+});
+
+trigger.addEventListener("click", () => {
+  toggleModal();
 });
