c71ffe34abd90e04302789f6e0c0282e8e2af60c
Harry Northam <harrynortham@icloud.com>
2019-05-11 14:19:26 +0200

    traffic toggle menu added

--- js/app.js
+++ js/app.js
@@ -14,10 +14,28 @@ if (alertClicked === "true") {
   });
 }
 
+//modal box
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+closeButton.addEventListener("click", () => {
+  toggleModal();
+});
+
+trigger.addEventListener("click", () => {
+  toggleModal();
+});
+
 //charts
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
   type: "bar",
   data: {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -65,16 +83,8 @@ var myChart = new Chart(ctx, {
   }
 });
 
-//get navigation items
-
-//if clicked create a new chart and set menu item active
-
-//if menu item is active don't create a new chart on clicked
-
-//maybe use case
-
-var ctx2 = document.getElementById("myChart2").getContext("2d");
-var myPieChart = new Chart(ctx2, {
+const ctx2 = document.getElementById("myChart2").getContext("2d");
+const myPieChart = new Chart(ctx2, {
   type: "doughnut",
   data: {
     labels: ["Phones", "Tablets", "Desktops"],
@@ -102,20 +112,182 @@ var myPieChart = new Chart(ctx2, {
   }
 });
 
-//modal box
+const ctx3 = document.getElementById("myChart3").getContext("2d");
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
+const hourlyTrafficChart = {
+  type: "line",
+  data: {
+    labels: [
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+      "21:00",
+      "22:00",
+      "23:00",
+      "00:00"
+    ],
+    datasets: [
+      {
+        data: [
+          600,
+          800,
+          1900,
+          1300,
+          1000,
+          1200,
+          1700,
+          1500,
+          1800,
+          1700,
+          2100,
+          1900,
+          2200
+        ],
+        backgroundColor: "rgba(115, 119, 191, 0.5)",
+        borderColor: "#7377bf",
+        borderWidth: 0.5
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    elements: { line: { tension: 0 }, point: { radius: 5 } }
+  }
+};
+const dailyTrafficChart = {
+  type: "line",
+  data: {
+    labels: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ],
+    datasets: [
+      {
+        data: [200, 1900, 2200, 1000, 1200, 1500, 1700],
+        backgroundColor: "rgba(115, 119, 191, 0.5)",
+        borderColor: "#7377bf",
+        borderWidth: 0.5
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    elements: { line: { tension: 0 }, point: { radius: 5 } }
+  }
+};
+const weeklyTrafficChart = {
+  type: "line",
+  data: {
+    labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+    datasets: [
+      {
+        data: [1000, 800, 1900, 300],
+        backgroundColor: "rgba(115, 119, 191, 0.5)",
+        borderColor: "#7377bf",
+        borderWidth: 0.5
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    elements: { line: { tension: 0 }, point: { radius: 5 } }
+  }
+};
+const monthlyTrafficChart = {
+  type: "line",
+  data: {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ],
+    datasets: [
+      {
+        data: [
+          100,
+          800,
+          1900,
+          300,
+          1000,
+          1200,
+          1700,
+          500,
+          1800,
+          1700,
+          2100,
+          200
+        ],
+        backgroundColor: "rgba(115, 119, 191, 0.5)",
+        borderColor: "#7377bf",
+        borderWidth: 0.5
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    elements: { line: { tension: 0 }, point: { radius: 5 } }
+  }
+};
 
-function toggleModal() {
-  modal.classList.toggle("show-modal");
+new Chart(ctx3, hourlyTrafficChart);
+document.querySelector("#hourlyTrafficChart").classList.add("active");
+
+const trafficNav = document.querySelector(".traffic-nav");
+
+function toggleActive(linkId) {
+  const trafficNavItem = trafficNav.getElementsByTagName("li");
+  const myLink = document.getElementById(linkId);
+  for (let i = 0; i < trafficNavItem.length; ++i) {
+    trafficNavItem[i].getElementsByTagName("a")[0].classList.remove("active");
+  }
+  myLink.className = "active";
 }
 
-closeButton.addEventListener("click", () => {
-  toggleModal();
-});
-
-trigger.addEventListener("click", () => {
-  toggleModal();
+trafficNav.addEventListener("click", e => {
+  linkId = e.target.id;
+  if (linkId === "hourlyTrafficChart") {
+    new Chart(ctx3, hourlyTrafficChart);
+    toggleActive(linkId);
+  } else if (linkId === "dailyTrafficChart") {
+    new Chart(ctx3, dailyTrafficChart);
+    toggleActive(linkId);
+  } else if (linkId === "weeklyTrafficChart") {
+    new Chart(ctx3, weeklyTrafficChart);
+    toggleActive(linkId);
+  } else if (linkId === "monthlyTrafficChart") {
+    new Chart(ctx3, monthlyTrafficChart);
+    toggleActive(linkId);
+  }
 });
